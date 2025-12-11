270bc4bcd66be71ccab0b7df01e9808eaff8d8e3
EFRAIN GARCIA ALARCON <112557@alumnouninter.mx>
2025-12-10 18:26:47 -0600

    Cambios finales del proyecto

--- SistemaAPI/Program.cs
+++ SistemaAPI/Program.cs
@@ -4,11 +4,10 @@ using SistemaAPI.Data;
 using SistemaAPI.Commands;
 
 #region Verificar Comandos CLI
-// Verificar si se está ejecutando un comando CLI antes de iniciar la aplicación web
 if (args.Length > 0 && args.Contains("cleardb"))
 {
     await ClearDatabaseCommand.ExecuteAsync(args);
-    return; // Salir después de ejecutar el comando
+    return;
 }
 #endregion
 
